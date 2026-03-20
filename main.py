710c4a8ab715bda04dd98e9c93e0cabedd0f7844
belforz <macedobeiramar@hotmail.com>
2026-03-20 16:04:10 -0300

    feature: add anchors to json v2

--- main.py
+++ main.py
@@ -5,10 +5,23 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 # from infrastructure.adapters.mistral_connector import MistralConnector
 # from infrastructure.adapters.spacy_adapter import SpacyAdapter
+from infrastructure.adapters.sentence_transformer_adapter import SentenceTransformerAdapter
 
 
 def main():
-    print("algo")
+    connector = SentenceTransformerAdapter()
+    
+    sentences = [
+    "The weather is lovely today.",
+    "It's so sunny outside!",
+    "He drove to the stadium."
+]
+    embeddings = connector.generate_embeddings(texts=sentences)
+    for sentence in zip(sentences, embeddings):
+        print("Texto", sentence)
+        print("Embeddings", embeddings)
+    
+    
     # processor = SpacyAdapter()
     # text = " O Banco do Brasil anunciou lucros recordes em Brasília. "
     # entities = processor.extract_entities(text)
@@ -18,8 +31,8 @@ def main():
     #     "Marcos viajou para Portugal.",
     #     "A Apple lançou um novo celular hoje."
     # ] * 1000
-    # results = processor.extract_batchs_entities(batch_texts, batch_size=50)
-    # print("Result of first index:", results[4])
+    # embeddingss = processor.extract_batchs_entities(batch_texts, batch_size=50)
+    # print("embeddings of first index:", embeddingss[4])
 
 
 if __name__ == "__main__":
--- src/infrastructure/adapters/sentence_transformer_adapter.py
+++ src/infrastructure/adapters/sentence_transformer_adapter.py
@@ -4,14 +4,25 @@ from sentence_transformers import SentenceTransformer
 import numpy as np
 
 class SentenceTransformerAdapter:
-    def __init__(self, model_name: str = 'paraphrase-multilingual-MiniLM-L12-v2', prompt: str = None):
+    def __init__(self, model_name: str = 'paraphrase-multilingual-MiniLM-L12-v2'):
         self._model = SentenceTransformer(model_name)
-        self.prompt = prompt
     
-    def generate_embeddings(self, texts: Union[str, List[str]]) -> np.ndarray:
-        if isinstance(texts, str):
+    def generate_embeddings(self, texts: Union[str, List[str]]) -> Union[np.ndarray, List[float]]:
+        """ Generate embeddings for a single text or a list of texts
+
+        Args:
+            texts (Union[str, List[str]]): _description_
+        
+        Returns:
+            Union[np.ndarray, List[float]]: _description_
+        
+        """
+        is_single_string = isinstance(texts, str)
+        if is_single_string:
             texts = [texts]
         embeddings = self._model.encode(texts, convert_to_numpy=True)
+        if is_single_string:
+            return embeddings[0]
         return embeddings
     
     
